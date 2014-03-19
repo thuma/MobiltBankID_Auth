4aaffa3317db41b45e2531eb1e78331e20ede664
Martin Thuresson <martin.thure@gmail.com>
2014-03-19 22:14:05 +0100

    found that oderRef is not in doc

--- bankid.py
+++ bankid.py
@@ -1,13 +1,13 @@
 import tornado.httpclient
 
 http_client = tornado.httpclient.HTTPClient()
+request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Mobile", method='GET', validate_cert=0)
 try:
-    response = http_client.fetch("https://test.bankid.com/Mobile")
+    response = http_client.fetch(request)
     kaka = response.headers['Set-Cookie']
 except tornado.httpclient.HTTPError as e:
     print "Error:", e
 http_client.close()
-
 
 print kaka
 kakor = kaka.split(',')
@@ -20,29 +20,66 @@ for i in kakor:
 
 print allakakor
 
-request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step1", method='POST', headers='Cookie:'+allakakor[0]+';'+allakakor[1]+';'+allakakor[2]+';'+allakakor[3], body='CenturyBreak=2001&Personnummer=198303234655')
+headers = tornado.httputil.HTTPHeaders({"Cookie": allakakor[0]+';'+allakakor[1]+';'+allakakor[2]+';'+allakakor[3]})
 
+request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step1", method='POST',headers =headers , body='CenturyBreak=2001&Personnummer=198303234655', validate_cert=0)
 http_client = tornado.httpclient.HTTPClient()
 try:
     response = http_client.fetch(request)
 except tornado.httpclient.HTTPError as e:
     print "Error:", e
-
+    
+print response.body
 http_client.close()
-'''Cookies:
-https://test.bankid.com/Mobile
 
-Pno:
-curl 'https://test.bankid.com/Authentication/Step1' 
--H 'Cookie: ASP.NET_SessionId=lawgyt0ykjm55eyygtjdl5xp; LanguageCookie=sv; __RequestVerificationToken=4P1JvWs1ejiEyDi32QluJdRRtH-FgMQ7sJxUiOolhezR7LvkUY24wX8UkXdxDNYmo5z5p9L9ZF1KrIBtGybXseLMEAph86k1sxsufWob69iYQo8yyr71Klpy1yE2tYbxqbVaqAI4Rzze1tdM9kq8Dw2; DisplaySettings=F7=OK' 
--H 'Origin: https://test.bankid.com' 
--H 'Accept-Encoding: gzip,deflate,sdch' 
--H 'Accept-Language: sv-SE,sv;q=0.8,en-US;q=0.6,en;q=0.4' 
--H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/32.0.1700.107 Chrome/32.0.1700.107 Safari/537.36' 
--H 'Content-Type: application/x-www-form-urlencoded' 
- --data 'CenturyBreak=2001&Personnummer=198303234655'
 
-curl 'https://test.bankid.com/Authentication/Step2RpCall' -H 'Accept: */*' 
+headers = tornado.httputil.HTTPHeaders({"Cookie": allakakor[0]+';LanguageCookie=sv;'+allakakor[2]+';'+allakakor[3]})
+
+request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step2RpCall", method='POST', body = " ", headers=headers, validate_cert=0)
+http_client = tornado.httpclient.HTTPClient()
+try:
+    response = http_client.fetch(request)
+except tornado.httpclient.HTTPError as e:
+    print "Error:", e
+    
+print response.body
+http_client.close()
+
+exit()
+
+headers = tornado.httputil.HTTPHeaders({"Cookie": allakakor[0]+';'+allakakor[1]+';'+allakakor[2]+';'+allakakor[3]})
+
+request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step3", method='GET',headers=headers, validate_cert=0)
+http_client = tornado.httpclient.HTTPClient()
+try:
+    response = http_client.fetch(request)
+except tornado.httpclient.HTTPError as e:
+    print "Error:", e
+    
+print response.body
+http_client.close()
+
+request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step3RpCall", method='GET',headers=headers, validate_cert=0)
+http_client = tornado.httpclient.HTTPClient()
+try:
+    response = http_client.fetch(request)
+except tornado.httpclient.HTTPError as e:
+    print "Error:", e
+    
+print response.body
+http_client.close()
+
+request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step4", method='GET',headers=headers, validate_cert=0)
+http_client = tornado.httpclient.HTTPClient()
+try:
+    response = http_client.fetch(request)
+except tornado.httpclient.HTTPError as e:
+    print "Error:", e
+    
+print response.body
+http_client.close()
+
+'''curl 'https://test.bankid.com/Authentication/Step2RpCall' -H 'Accept: */*' 
 -H 'Referer: https://test.bankid.com/Authentication/Step1' 
 -H 'Origin: https://test.bankid.com' 
 -H 'X-Requested-With: XMLHttpRequest' 
@@ -84,5 +121,19 @@ curl 'https://test.bankid.com/Authentication/Step4'
 -H 'Connection: keep-alive' 
 --data 'orderRef=7c8174ff-ea0d-4542-9227-5813bfb3f084' --compressed'''
 
+
+'''Cookies:
+https://test.bankid.com/Mobile
+
+Pno:
+curl 'https://test.bankid.com/Authentication/Step1' 
+-H 'Cookie: ASP.NET_SessionId=lawgyt0ykjm55eyygtjdl5xp; LanguageCookie=sv; __RequestVerificationToken=4P1JvWs1ejiEyDi32QluJdRRtH-FgMQ7sJxUiOolhezR7LvkUY24wX8UkXdxDNYmo5z5p9L9ZF1KrIBtGybXseLMEAph86k1sxsufWob69iYQo8yyr71Klpy1yE2tYbxqbVaqAI4Rzze1tdM9kq8Dw2; DisplaySettings=F7=OK' 
+-H 'Origin: https://test.bankid.com' 
+-H 'Accept-Encoding: gzip,deflate,sdch' 
+-H 'Accept-Language: sv-SE,sv;q=0.8,en-US;q=0.6,en;q=0.4' 
+-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/32.0.1700.107 Chrome/32.0.1700.107 Safari/537.36' 
+-H 'Content-Type: application/x-www-form-urlencoded' 
+ --data 'CenturyBreak=2001&Personnummer=198303234655
+'''
 
 
