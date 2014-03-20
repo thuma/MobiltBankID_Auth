437183f23ec06d0c38fa290479834e1063df8aed
Martin Harari Thuresson <martin.thure@gmail.com>
2014-03-20 11:55:44 +0100

    working all the way with info comments

--- bankid.py
+++ bankid.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
+import sys
 import tornado.httpclient
 from bs4 import BeautifulSoup
+
+pno = sys.argv[1]
 
 http_client = tornado.httpclient.HTTPClient()
 request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Mobile", method='GET', validate_cert=0)
@@ -19,11 +23,11 @@ for i in kakor:
 	if(i.split(';')[0][0] != ' '):
 		allakakor.append(i.split(';')[0])
 
-print allakakor
+print 'Kakor hämtade -> Söker'
 
 headers = tornado.httputil.HTTPHeaders({"Cookie": allakakor[0]+';LanguageCookie=sv;'+allakakor[2]+';'+allakakor[3]})
 
-request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step1", method='POST',headers =headers , body='CenturyBreak=2001&Personnummer=198303234655', validate_cert=0)
+request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step1", method='POST',headers =headers , body='CenturyBreak=2001&Personnummer='+pno, validate_cert=0)
 http_client = tornado.httpclient.HTTPClient()
 try:
     response = http_client.fetch(request)
@@ -35,6 +39,8 @@ http_client.close()
 
 orderRef =  "orderRef="+html_doc.find(id="orderRef")['value']
 reqtok =  "__RequestVerificationToken="+html_doc.find(id="nextStepForm").findChildren()[0]['value']
+
+print 'Sök startad -> order ref hämtad -> Väntar'
 
 print orderRef + reqtok
 headers = tornado.httputil.HTTPHeaders({"Cookie": allakakor[0]+';LanguageCookie=sv;'+allakakor[2]+';'+allakakor[3]})
@@ -48,6 +54,8 @@ except tornado.httpclient.HTTPError as e:
     
 http_client.close()
 
+print 'Användaren inloggad'
+
 headers = tornado.httputil.HTTPHeaders({"Cookie": allakakor[0]+';;LanguageCookie=sv;'+allakakor[2]+';'+allakakor[3]})
 
 request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step3", method='POST', body = orderRef +'&'+reqtok, headers=headers, validate_cert=0)
@@ -58,6 +66,8 @@ except tornado.httpclient.HTTPError as e:
     print "Error:", e
 http_client.close()
 
+print 'Användaren autentiserar'
+
 request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step3RpCall", method='POST',body = orderRef, headers=headers, validate_cert=0)
 http_client = tornado.httpclient.HTTPClient()
 try:
@@ -66,71 +76,20 @@ except tornado.httpclient.HTTPError as e:
     print "Error:", e
 http_client.close()
 
+print 'Väntar'
+
 request = tornado.httpclient.HTTPRequest("https://test.bankid.com/Authentication/Step4", method='POST',body = orderRef, headers=headers, validate_cert=0)
 http_client = tornado.httpclient.HTTPClient()
 try:
     response = http_client.fetch(request)
 except tornado.httpclient.HTTPError as e:
     print "Error:", e
-    
-print response.body
+
+html_doc = BeautifulSoup(response.body)
+part1 = html_doc.find(id="MainBody")
+part1 = part1.find_all('h2')
+
+print part1[0].get_text()
 http_client.close()
-
-'''curl 'https://test.bankid.com/Authentication/Step2RpCall' -H 'Accept: */*' 
--H 'Referer: https://test.bankid.com/Authentication/Step1' 
--H 'Origin: https://test.bankid.com' 
--H 'X-Requested-With: XMLHttpRequest' 
--H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/32.0.1700.107 Chrome/32.0.1700.107 Safari/537.36' 
--H 'Content-Type: application/x-www-form-urlencoded' 
---data 'orderRef=7c8174ff-ea0d-4542-9227-5813bfb3f084' --compressed
-
-curl 'https://test.bankid.com/Authentication/Step3' 
--H 'Cookie: ASP.NET_SessionId=twqr3xam4y1ive0fuyyjzxvp; LanguageCookie=sv; __RequestVerificationToken=bxzuSvnHmoHmA1Huxem-SHx-yUHTdG-g8boMVl-OTvuhSZNYAunfrRABPBWWBQdwV5AQc6DYv5wFe51BCkEY0qZPttWq-EIfVqGIDLqHhjJz0BU-vsnC-FLQV9I_qI7pvN1mzDFTEF14FgtVcrSIyw2; DisplaySettings=' 
--H 'Origin: https://test.bankid.com' 
--H 'Accept-Encoding: gzip,deflate,sdch' 
--H 'Accept-Language: sv-SE,sv;q=0.8,en-US;q=0.6,en;q=0.4' 
--H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/32.0.1700.107 Chrome/32.0.1700.107 Safari/537.36' 
--H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' 
--H 'Cache-Control: max-age=0' 
--H 'Referer: https://test.bankid.com/Authentication/Step1' 
--H 'Connection: keep-alive' 
---data '__RequestVerificationToken=ymjX8g1-AHdoDMsnTQJmpNmoYB9PDJ2e2GmoAqlSz7zGwiuWPEqQf-Fv90S7b7QusQKvn-psd9DEtcrjnlSZgCcPFgW1DwSkOa5KrvzyqOFLdPXao0z2qutYjbx3G1ygsQrjzI2Lt-PbS-ry5fjcTnYeSRTrWFCRCD08w4IWUPU1&orderRef=7c8174ff-ea0d-4542-9227-5813bfb3f084' --compressed
-
-curl 'https://test.bankid.com/Authentication/Step3RpCall' 
--H 'Accept: */*' 
--H 'Referer: https://test.bankid.com/Authentication/Step3' 
--H 'Origin: https://test.bankid.com' 
--H 'X-Requested-With: XMLHttpRequest' 
--H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/32.0.1700.107 Chrome/32.0.1700.107 Safari/537.36' 
--H 'Content-Type: application/x-www-form-urlencoded' 
---data 'orderRef=7c8174ff-ea0d-4542-9227-5813bfb3f084' --compressed
-
-curl 'https://test.bankid.com/Authentication/Step4' 
--H 'Cookie: ASP.NET_SessionId=twqr3xam4y1ive0fuyyjzxvp; LanguageCookie=sv; __RequestVerificationToken=bxzuSvnHmoHmA1Huxem-SHx-yUHTdG-g8boMVl-OTvuhSZNYAunfrRABPBWWBQdwV5AQc6DYv5wFe51BCkEY0qZPttWq-EIfVqGIDLqHhjJz0BU-vsnC-FLQV9I_qI7pvN1mzDFTEF14FgtVcrSIyw2; DisplaySettings=' 
--H 'Origin: https://test.bankid.com' 
--H 'Accept-Encoding: gzip,deflate,sdch' 
--H 'Accept-Language: sv-SE,sv;q=0.8,en-US;q=0.6,en;q=0.4' 
--H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/32.0.1700.107 Chrome/32.0.1700.107 Safari/537.36' 
--H 'Content-Type: application/x-www-form-urlencoded' 
--H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' 
--H 'Cache-Control: max-age=0' 
--H 'Referer: https://test.bankid.com/Authentication/Step3' 
--H 'Connection: keep-alive' 
---data 'orderRef=7c8174ff-ea0d-4542-9227-5813bfb3f084' --compressed'''
-
-
-'''Cookies:
-https://test.bankid.com/Mobile
-
-Pno:
-curl 'https://test.bankid.com/Authentication/Step1' 
--H 'Cookie: ASP.NET_SessionId=lawgyt0ykjm55eyygtjdl5xp; LanguageCookie=sv; __RequestVerificationToken=4P1JvWs1ejiEyDi32QluJdRRtH-FgMQ7sJxUiOolhezR7LvkUY24wX8UkXdxDNYmo5z5p9L9ZF1KrIBtGybXseLMEAph86k1sxsufWob69iYQo8yyr71Klpy1yE2tYbxqbVaqAI4Rzze1tdM9kq8Dw2; DisplaySettings=F7=OK' 
--H 'Origin: https://test.bankid.com' 
--H 'Accept-Encoding: gzip,deflate,sdch' 
--H 'Accept-Language: sv-SE,sv;q=0.8,en-US;q=0.6,en;q=0.4' 
--H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/32.0.1700.107 Chrome/32.0.1700.107 Safari/537.36' 
--H 'Content-Type: application/x-www-form-urlencoded' 
- --data 'CenturyBreak=2001&Personnummer=198303234655
-'''
 
 
